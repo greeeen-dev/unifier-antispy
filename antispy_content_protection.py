825834f8e6c2d61da7551e040381c80c0ae80bc5
green <41323182+greeeen-dev@users.noreply.github.com>
2024-05-18 16:55:31 +0200

    Update and rename template_check.py to antispy_content_protection.py

--- antispy_content_protection.py
+++ antispy_content_protection.py
@@ -1,6 +1,6 @@
 """
-<one line to give the program's name and a brief idea of what it does.>
-Copyright (C) <year>  <name of author>
+Unifier AntiSpy - Data scraper detection plugin for Unifier
+Copyright (C) 2024  Green
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
